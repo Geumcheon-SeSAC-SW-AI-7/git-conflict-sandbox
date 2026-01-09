e1aae8a16da540c01eaf9cfdf0acaf4f786928d8
Jongwoo Shin <jongwoo108@gmail.com>
2026-01-09 16:55:49 +0900

    해결

--- main.py
+++ main.py
@@ -2,3 +2,32 @@ rows, cols = len(grid), len(grid[0])
 count = 0
 visited = set()
 
+rows, cols = len(grid), len(grid[0])
+count = 0
+visited = set()
+def num_islands(grid):
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    visited = set()
+    count = 0
+
+    def dfs(r, c):
+        stack = [(r, c)]
+        while stack:
+            cr, cc = stack.pop()
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = cr + dr, cc + dc
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '1' and (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    stack.append((nr, nc))
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == '1' and (r, c) not in visited:
+                visited.add((r, c))
+                dfs(r, c)
+                count += 1
+
+    return count
